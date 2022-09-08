10c15454d5e6a0c0acfe2686525392680b63a19c
John Melody Me <Johnmelodyme@yandex.com>
2022-09-08 23:04:21 +0800

    change output

--- Sources/LogToDiscord/LogToDiscord.swift
+++ Sources/LogToDiscord/LogToDiscord.swift
@@ -27,16 +27,16 @@ public struct LogToDiscord
         switch(level)
         {
             case Level.Info:
-                return "```[* Info] \n => \(message)```"
+                return "```**[Info]** \n => \(message)```"
                 
             case Level.Error:
-                return "```[* Error] \n \(message)```"
+                return "```**[Error]** \n \(message)```"
             
             case Level.Debug:
-                return "```[* Debug] \n \(message)```"
+                return "```**[Debug]** \n \(message)```"
             
             case Level.Warn:
-                return "```[* Warning] \n \(message)```"
+                return "```**[Warning]** \n \(message)```"
         }
     }
     
