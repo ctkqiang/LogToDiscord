82414425e4555ba6b2f1e0ca43fdeee8ee719fa6
John Melody Me <Johnmelodyme@yandex.com>
2022-09-08 22:41:29 +0800

    remove fro macos 10.10

--- example/Sources/example/main.swift
+++ example/Sources/example/main.swift
@@ -1,2 +1,4 @@
 import Foundation
+import LogToDiscord
 
+try LogToDiscord(appName: "testing", webhookUrl: "https://discord.com/api/webhooks/923568801759506463/OwCxiGYKZpsFZZLB51nXtWo6rkK8U3oNtZU6u9kSiEHh60G5kEc87igKbYZ7gJwaDC-D").send(message: "testing", level: Level.Info, delay: 10)
